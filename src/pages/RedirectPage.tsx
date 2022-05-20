edc247d6284bfaa80f91b1ced7db85ef4e3bac4b
kimdobie <kimdobie@gmail.com>
2022-05-20 11:44:01 -0500

    Template for 2.1.0
    update depenencies
    
    Add cypress
    
    Format json on University page
    
    Examples
    
    Update templates and action files
    
    Add skipmenu

--- src/pages/RedirectPage.tsx
+++ src/pages/RedirectPage.tsx
@@ -1,14 +1,7 @@
-/* NOTE:
-
-There are two ways to automatically direct a user to a given route:
-1. Use the useNavigate Hook 
-2. Use Navigate component (less ideal)
-*/
-
 import React, { ReactElement, useEffect, useState } from 'react';
 import { Row, Col, Button } from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom'; // Needed for method 1
-// import {  Navigate, useNavigate  } from 'react-router'; // Needed for method 2
+import { useNavigate } from 'react-router-dom';
+
 import { Link } from 'react-router-dom';
 
 import ROUTES from '../AppRouteNames';
@@ -21,7 +14,6 @@ const RedirectPage = (): ReactElement => {
   useEffect(() => {
     if (count <= 0) {
       // EXAMPLE: Automatically forwarding to a route (without a button click)
-      // For method 1
       return navigate(ROUTES.HOME);
       return;
     }
@@ -44,8 +36,6 @@ const RedirectPage = (): ReactElement => {
             You will automatically be directed to the home page in {count}{' '}
             seconds!
           </p>
-          {/* Redirect component only used in method 2 */}
-          {/* {count <= 0 ? <Navigate to={ROUTES.HOME} /> : null} */}
           <p>
             <Link to={ROUTES.HOME}>Go to home page</Link>
           </p>
