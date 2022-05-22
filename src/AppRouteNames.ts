1acccb04adc25442fbc5df0b7c2a876410aab731
kimdobie <kimdobie@gmail.com>
2022-05-21 21:40:41 -0500

    more deletes

--- src/AppRouteNames.ts
+++ src/AppRouteNames.ts
@@ -2,8 +2,4 @@ export default {
   HOME: '/',
   VERSION: '/version',
   FEATURE_FLAGS: '/features',
-  COLOR: '/color',
-  COLOR_PARAMS: { COLOR_NAME: 'colorName' },
-  REDIRECT: '/redirect',
-  UNIVERSITIES: '/universities',
 };
