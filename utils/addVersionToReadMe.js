3627f7d3ab521c13b2ef667e30636fe5c1bca6fc
kimdobie <kimdobie@gmail.com>
2022-08-01 16:58:46 -0500

    Update add version to Readme

--- utils/addVersionToReadMe.js
+++ utils/addVersionToReadMe.js
@@ -11,7 +11,10 @@ const { version } = packageJson;
 const readMe = fs.readFileSync(path.join(__dirname, '../README.md'));
 
 let newReadMe = readMe.toString().split('\n');
-newReadMe[newReadMe.length - 1] = `Based on template version ${version}`;
+while (newReadMe[newReadMe.length - 1] === '') {
+  newReadMe.pop();
+}
+newReadMe[newReadMe.length - 1] = `Based on template version ${version}\n`;
 
 newReadMe = newReadMe.join('\n');
 
