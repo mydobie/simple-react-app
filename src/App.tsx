b60ea9d0fe623d422a1f8366071f3be7db166b32
kimdobie <kimdobie@gmail.com>
2022-07-31 20:57:53 -0500

    Update dependancies

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@
 import React, { ReactElement, useReducer } from 'react';
 import { BrowserRouter, HashRouter } from 'react-router-dom'; // Use `HashRouter as Router` when you can't control the URL ... like GitHub pages
 import { Container, Card } from 'react-bootstrap';
+import SkipMenu from 'skip-menu-react';
 
 const Router =
   process.env.REACT_APP_USE_HASH_ROUTER === 'true' ? HashRouter : BrowserRouter;
 
 // START FEATURE FLAGS
-import { loadFeatureFlags } from 'feature-flags/react';
+import { loadFeatureFlags, isFeatureActive } from 'feature-flags/react';
 import { featureFlagArray } from './feature-flags.config';
 // END FEATURE FLAGS
 
@@ -19,7 +20,8 @@ import SetAxios from './components/SetAxios';
 import './scss/index.scss';
 
 const Header = (): ReactElement => (
-  <header>
+  <header data-testid='header'>
+    <SkipMenu theme='bootstrap' useAccessKey />
     <Card bg='dark' text='white'>
       <Card.Body>
         <Card.Title>Sample react application</Card.Title>
@@ -28,7 +30,23 @@ const Header = (): ReactElement => (
   </header>
 );
 
-const Footer = (): ReactElement => <footer></footer>;
+const Footer = (): ReactElement => {
+  // EXAMPLE: Show/Hide based on feature flag
+  const isColors = isFeatureActive('COLORS');
+  return (
+    <footer>
+      <Card bg='light' style={{ marginTop: '20px' }}>
+        {isColors ? (
+          <>
+            <Card.Body>
+              <strong>Colors:</strong> Red, Orange, Yellow, Green, Blue, Violet
+            </Card.Body>
+          </>
+        ) : null}
+      </Card>
+    </footer>
+  );
+};
 
 const App = (): ReactElement => {
   const basename = '';
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,19 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+
 /* This tells the application to load into the html object with an id of "root"
 NOTE: There normally isn't a reason to change this file
 */
 
-import React, { ReactElement } from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import './scss/index.scss';
 
-const RenderApp: ReactElement = <App />;
+const container = document.getElementById('root');
+const root = createRoot(container!);
 
-ReactDOM.render(RenderApp, document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
